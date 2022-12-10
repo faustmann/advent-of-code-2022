898023aa47bfc2e116bf0fd5dc0ead6307b1ba22
Faustmann <faustmann@gmail.com>
2022-12-10 17:32:37 +0100

    10

---
+++ 10/1.py
@@ -0,0 +1,34 @@
+from functools import reduce
+
+with open('1.txt') as fp:
+    commands = []
+    for command_split in map(lambda line: line.split(' '), (fp.read().splitlines())):
+        command = {}
+        command['instruction'] = command_split[0]
+        if len(command_split) == 2:
+            commands.append({'instuction': 'proc_addx'})
+            command['summand'] = int(command_split[1])
+
+        commands.append(command)
+
+executed_com_res = reduce(lambda acc, elem: acc + [
+    {**elem, **{'sum': acc[-1]['sum'] + elem.get('summand', 0)}}], commands, [{'sum': 1}])
+
+fst_part_result = sum(idx * executed_com_res[idx-1]['sum']
+                      for idx in range(20, len(executed_com_res), 40))
+print(f'{fst_part_result=}')
+
+output = ''
+for idx, cur_exec_command in enumerate(executed_com_res):
+    cursor_pos_x = idx % 40
+    if cursor_pos_x == 0:
+        output += '\n'
+
+    if abs(cursor_pos_x - cur_exec_command['sum']) <= 1:
+        output += '#'
+    else:
+        output += '.'
+
+
+print(output)
+print('fin')
