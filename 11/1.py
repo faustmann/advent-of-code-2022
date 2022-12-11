6b3de52acf68217ccba60eb02bdc2d705a212c73
Faustmann <faustmann@gmail.com>
2022-12-11 13:38:53 +0100

    snd 11: currently too slow

--- 11/1.py
+++ 11/1.py
@@ -5,14 +5,22 @@ CALC_FST_PART = False
 with open('1.txt') as fp:
     monkey_str_list = fp.read().split('\n\n')
 
-
-def create_calc(fst, op, snd):
-    if calc_parts[1] == '*':
-        return lambda old: (int(fst) if fst.isnumeric(
-        ) else old) * (int(snd) if snd.isnumeric() else old)
-    else:
-        return lambda old: (int(fst) if fst.isnumeric(
-        ) else old) + (int(snd) if snd.isnumeric() else old)
+if CALC_FST_PART:
+    def create_calc(fst, op, snd):
+        if calc_parts[1] == '*':
+            return lambda old: (int(fst) if fst.isnumeric(
+            ) else old) * (int(snd) if snd.isnumeric() else old)
+        else:
+            return lambda old: (int(fst) if fst.isnumeric(
+            ) else old) + (int(snd) if snd.isnumeric() else old)
+else:
+    def create_calc(fst, op, snd):
+        if calc_parts[1] == '*':
+            return lambda old: [old[0], (int(fst) if fst.isnumeric(
+            ) else old[1]) * (int(snd) if snd.isnumeric() else old[1])]
+        else:
+            return lambda old: [None, (int(fst) if fst.isnumeric(
+            ) else old[1]) + (int(snd) if snd.isnumeric() else old[1])]
 
 
 monkeys = []
@@ -21,8 +29,12 @@ for monkey_str in monkey_str_list:
     monkeys.append(monkey)
     for line_idx, monkey_line in enumerate(monkey_str.split('\n')):
         if line_idx == 1:
-            monkey['items'] = [
-                int(item) for item in monkey_line.split(': ')[-1].split(', ')]
+            if CALC_FST_PART:
+                monkey['items'] = [
+                    int(item) for item in monkey_line.split(': ')[-1].split(', ')]
+            else:
+                monkey['items'] = [
+                    (None, int(item)) for item in monkey_line.split(': ')[-1].split(', ')]
         if line_idx == 2:
             calc_parts = monkey_line.split(' = ')[-1].split(' ')
             monkey['calc'] = create_calc(*calc_parts)
@@ -53,13 +65,19 @@ if CALC_FST_PART:
         f"fst past result {sorted_monkeys[0]['inspected_items'] * sorted_monkeys[1]['inspected_items']}")
 else:
     for round_num in range(10000):
-        print(f'round {round_num}')
+        print(f'{round_num=}')
         for monkey in monkeys:
             monkey['inspected_items'] += len(monkey['items'])
             while len(monkey['items']) != 0:
                 item = monkey['items'].pop(0)
                 item = monkey['calc'](item)
-                if item % monkey['div_test_number'] == 0:
+                for test_monkey in monkeys:
+                    if item[0] is None or not test_monkey['div_test_number'] in item[0]:
+                        if item[0] is None:
+                            item[0] = []
+                        if item[1] % test_monkey['div_test_number'] == 0:
+                            item[0].append(test_monkey['div_test_number'])
+                if monkey['div_test_number'] in item[0]:
                     itemToMonkey = monkey['test_true_monkey']
                 else:
                     itemToMonkey = monkey['test_false_monkey']
